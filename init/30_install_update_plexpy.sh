ff8f2ef746a9b0fe33cc33d998ee23d26b5cf6a8
sparklyballs <sparklyballs@gmail.com>
2016-01-18 14:40:46 +0000

    add opt out for autoupdates to git update init

--- init/30_install_update_plexpy.sh
+++ init/30_install_update_plexpy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-[[ ! -d /opt/plexpy/.git ] && (git clone https://github.com/drzoidberg33/plexpy.git /opt/plexpy && \
+[[ ! -d /opt/plexpy/.git ]] && (git clone https://github.com/drzoidberg33/plexpy.git /opt/plexpy && \
 chown -R abc:abc /config)
 
 # opt out for autoupdates
