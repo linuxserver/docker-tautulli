c21bbf8e07843dff7fe716bf421a1ae0683aa24f
zaggash <zaggash@users.noreply.github.com>
2016-02-19 18:40:51 +0100

    Add quote in the if statement

--- init/30_install_update_plexpy.sh
+++ init/30_install_update_plexpy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-[ $DEV ] && BRANCH="dev" || BRANCH="master"
+[ "$DEV" ] && BRANCH="dev" || BRANCH="master"
 echo "Cloning $BRANCH branch..."
 [[ ! -d /opt/plexpy/.git ]] && (git clone https://github.com/drzoidberg33/plexpy.git --branch $BRANCH /opt/plexpy && \
 chown -R abc:abc /config)
