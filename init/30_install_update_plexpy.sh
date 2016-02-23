8b793e7e8e2c367edd4699fffd9d91f57243d9ee
zaggash <zaggash@users.noreply.github.com>
2016-02-23 11:39:48 +0100

    Update 30_install_update_plexpy.sh

--- init/30_install_update_plexpy.sh
+++ init/30_install_update_plexpy.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-[ "$DEV" ] && BRANCH="dev" || BRANCH="master"
+#If variable $ADVANCED_DEV_BRANCH, set branch to DEV, otherwise, default to master
+[ "$ADVANCED_DEV_BRANCH" ] && BRANCH="dev" || BRANCH="master"
+#Display Warning message
+[ "$ADVANCED_DEV_BRANCH" ] && echo "BRANCH IS DEV, NO DOCKER SUPPORT WITH THIS BRANCH, THANKS"
 echo "Cloning $BRANCH branch..."
 [[ ! -d /opt/plexpy/.git ]] && (git clone https://github.com/drzoidberg33/plexpy.git --branch $BRANCH /opt/plexpy && \
 chown -R abc:abc /config)
