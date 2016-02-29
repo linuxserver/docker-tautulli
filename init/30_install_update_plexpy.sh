09c0869ac405973eb71c92e72d2c6f9a45e8b588
zaggash <zaggash@users.noreply.github.com>
2016-02-29 10:59:02 +0100

    Use a single variable.

--- init/30_install_update_plexpy.sh
+++ init/30_install_update_plexpy.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-#If variable $ADVANCED_GIT_BRANCH, set branch to $GIT_BRANCH, otherwise, default to master
-[ "$ADVANCED_GIT_BRANCH" ] && $GIT_BRANCH=$ADVANCED_GIT_BRANCH && \
-echo "BRANCH: $ADVANCED_GIT_BRANCH, Warning! This is unsupported."|| GIT_BRANCH="master"
+#If variable $ADVANCED_GIT_BRANCH, set branch to $ADVANCED_GIT_BRANCH, otherwise, default to master
+[ "$ADVANCED_GIT_BRANCH" ] && \
+echo "BRANCH: $ADVANCED_GIT_BRANCH, Warning! This is unsupported." || ADVANCED_GIT_BRANCH="master"
 
 
-[[ ! -d /opt/plexpy/.git ]] && (echo "Cloning $GIT_BRANCH branch..." && \
-git clone https://github.com/drzoidberg33/plexpy.git --branch $GIT_BRANCH /opt/plexpy && \
+[[ ! -d /opt/plexpy/.git ]] && (echo "Cloning $ADVANCED_GIT_BRANCH branch..." && \
+git clone https://github.com/drzoidberg33/plexpy.git --branch $ADVANCED_GIT_BRANCH /opt/plexpy && \
 chown -R abc:abc /config)
 
 # opt out for autoupdates
