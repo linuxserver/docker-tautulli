06938f7f9efa14d4809eaa50e3a63ef1a0cf0b28
zaggash <zaggash@users.noreply.github.com>
2016-03-08 13:37:55 +0100

    Edit chown orders

--- init/30_install_update_plexpy.sh
+++ init/30_install_update_plexpy.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
+chown -R abc:abc /config
+
 #If variable $ADVANCED_GIT_BRANCH, set branch to $ADVANCED_GIT_BRANCH, otherwise, default to master
 [ "$ADVANCED_GIT_BRANCH" ] && \
 echo "BRANCH: $ADVANCED_GIT_BRANCH, Warning! This is unsupported." || ADVANCED_GIT_BRANCH="master"
 
-
 [[ ! -d /opt/plexpy/.git ]] && (echo "Cloning $ADVANCED_GIT_BRANCH branch..." && \
 git clone https://github.com/drzoidberg33/plexpy.git --branch $ADVANCED_GIT_BRANCH /opt/plexpy && \
-chown -R abc:abc /config)
+chown -R abc:abc /opt/plexpy/)
 
 # opt out for autoupdates
 [ "$ADVANCED_DISABLEUPDATES" ] && exit 0
-
 cd /opt/plexpy
 git pull
-chown -R abc:abc /config
+chown -R abc:abc /opt/plexpy/
