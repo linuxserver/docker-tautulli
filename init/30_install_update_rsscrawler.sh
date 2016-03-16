79ab193eb71398ca02aa4a10da231202dcf523d7
RiX <jonathan.vogelbusch@gmail.com>
2016-03-16 08:15:05 +0100

    Update and rename 30_install_update_plexpy.sh to 30_install_update_rsscrawler.sh

--- init/30_install_update_rsscrawler.sh
+++ init/30_install_update_rsscrawler.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-[[ ! -d /opt/plexpy/.git ]] && (git clone https://github.com/drzoidberg33/plexpy.git /opt/plexpy && \
+[[ ! -d /config/.git ]] && (git clone https://github.com/rix1337/RSScrawler.git /config && \
 chown -R abc:abc /config)
 
 # opt out for autoupdates
 [ "$ADVANCED_DISABLEUPDATES" ] && exit 0
 
-cd /opt/plexpy
+cd /config
 git pull
 chown -R abc:abc /config
 
